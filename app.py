9e96e096d3463d97c7ea5dcf7f5d12ddd24becf1
Greg Harner <accounts@yongsa.net>
2024-02-09 08:16:23 -0500

    added container layout

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from firebase_admin import credentials
-from firebase_admin import firestore
-from flask import Flask, render_template, jsonify
+from firebase_admin import credentials, firestore
+from flask import Flask, render_template, jsonify, current_app
 from flask_cors import CORS
 import firebase_admin
 import json
@@ -37,6 +36,51 @@ def list_routes():
         methods = ','.join(rule.methods)
         routes.append({'endpoint': rule.endpoint, 'methods': methods, 'url': str(rule)})
     return routes
+
+@app.route("/v1/products", methods=["GET"])
+def get_products():
+    try:
+         
+        file_path = os.path.join(current_app.root_path, 'data', 'products.json')
+        print(file_path)        
+        
+        current_app.logger.info(f"Attempting to open file at: {file_path}")
+        
+        with open(file_path, 'r') as file:
+            products_data = json.load(file)
+            
+        return jsonify(products_data)
+    except FileNotFoundError as e:
+        current_app.logger.error(f"FileNotFoundError: {e}")
+        return jsonify({"error": "Products file not found"}), 404
+    except json.JSONDecodeError as e:
+        current_app.logger.error(f"JSONDecodeError: {e}")
+        return jsonify({"error": "Error decoding the products file"})
+
+@app.route("/v1/users", methods=["GET"])
+def get_users():
+    try:
+         
+        file_path = os.path.join(current_app.root_path, 'data', 'users.json')
+        print(file_path)        
+        
+        current_app.logger.info(f"Attempting to open file at: {file_path}")
+        
+        with open(file_path, 'r') as file:
+            products_data = json.load(file)
+            
+        return jsonify(products_data)
+    except FileNotFoundError as e:
+        current_app.logger.error(f"FileNotFoundError: {e}")
+        return jsonify({"error": "Products file not found"}), 404
+    except json.JSONDecodeError as e:
+        current_app.logger.error(f"JSONDecodeError: {e}")
+        return jsonify({"error": "Error decoding the products file"})
+
+
+@app.route("/test")
+def test_route():
+    return jsonify({"message": "This is a test."})
 
 def stringy_pretty(obj, description):
     class CircularReferenceEncoder(json.JSONEncoder):
